30f48d744757b61de7f58fc0d205a911a4f1f18e
guoyayong <guoyayong@tigerbrokers.com>
2019-11-26 10:14:03 +0800

    little change

--- string/Reverse String.java
+++ string/Reverse String.java
@@ -3,8 +3,8 @@ class Solution {
         int left = 0, right = s.length - 1;
         while (left < right) {
             char tmp = s[left];
-            s[left++] = s[right];
-            s[right--] = tmp;
+            s[left++] = s[right];//s[left] = s[right]; left++;
+            s[right--] = tmp;//s[right] = tmp; right--;
         }
     }
 }
